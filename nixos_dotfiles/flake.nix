86d1cb9f17d2d6cd68225de6945239377575a002
LuxySs <82043929+LuxySs@users.noreply.github.com>
2023-12-15 23:29:01 +0100

    Starter config
    Everything from Vimjoyer's video: "Ultimate NixOS Guide | Flakes | Home-Manager"

---
+++ nixos_dotfiles/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Nixos config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations = {
+        default = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit inputs;};
+          modules = [ 
+            ./hosts/default/configuration.nix
+          ];
+        };
+	#workmachine = nixpkgs.lib.nixosSystem {
+          #specialArgs = {inherit inputs;};
+          #modules = [ 
+            #./hosts/default/configuration.nix
+          #];
+        #};
+      };
+
+    };
+}
